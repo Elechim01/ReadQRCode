04e7068bd44a7d2ea4b7d6b12868b1c68d419767
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-06-19 18:04:51 +0200

    Initial Commit

---
+++ Shared/ReadQRCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadQRCodeApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 19/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadQRCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
