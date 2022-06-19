9d2c847227082d797344f686d02dbf3e70261f69
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-06-19 20:23:53 +0200

    first commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,45 @@
 //
 
 import SwiftUI
+import CodeScanner
+import UniformTypeIdentifiers
 
 struct ContentView: View {
+    @State var text: String = "..."
+    @State var isShowingScanner = false
+    @Environment(\.colorScheme) var colorScheme
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button {
+                isShowingScanner.toggle()
+            } label: {
+                Text("ReadQrCode")
+            }
+            Menu(text){
+                Button {
+                    UIPasteboard.general.setValue(text,forPasteboardType: UTType.plainText.identifier)
+                } label: {
+                    Text("Copia")
+                    
+                }
+            }
+            
+            
+            .foregroundColor(colorScheme == .dark ? .white : .black)
+        }
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(codeTypes: [.qr], completion: handleScan(result:))
+        }
+    }
+    func handleScan(result: Result<ScanResult,ScanError>){
+        self.isShowingScanner.toggle()
+        switch result{
+        case.success(let result):
+            text = result.string
+        case .failure(let error):
+            text = "Screen fallied" + error.localizedDescription
+            
+        }
     }
 }
 
